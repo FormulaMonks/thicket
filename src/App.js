40f5f71ee632951f0546c458661659e8c80efc83
Gorka <gorka@aquigorka.com>
2017-09-28 14:24:18 +0200

    Add prototype gifs from user camera

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import classname from 'classname'
+import Giffer from './giffer'
+import './App.css'
+import cameraIcon from './camera.png'
 
 class App extends Component {
+  
+  state = { status: '' }
+
   render() {
+    const { status } = this.state
+    let giffer = null
+    if (status === 'shoot') {
+      giffer = <Giffer onCancel={() => this.setState({ status: '' })} />
+    }
+    const footerStyles = classname('footer', { 'footer-active': status !== 'shoot' })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <div className="header">Thicket</div>
+        <div className="content">
+          <div className="stream-wrapper">User feed</div>
+          <div className={classname('giffer-wrapper', { 'giffer-wrapper-active': status === 'shoot' })}>{giffer}</div>
+        </div>
+        <div className={footerStyles}>
+          <div className="camera-icon-wrapper" onClick={() => this.setState({ status: 'shoot' })}>
+            <img className="camera-icon" src={cameraIcon} alt="New GIF" />
+          </div>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ src/giffer/index.js
@@ -0,0 +1,161 @@
+import React, { Component } from 'react'
+import gifshot from 'gifshot'
+import Spinner from 'react-spinkit'
+import classname from 'classname'
+import './styles.css'
+import acceptIcon from './accept.png'
+import againIcon from './again.png'
+import shootIcon from './shoot.png'
+import cancelIcon from './cancel.png'
+
+const GIF_DURATION = 3000
+const GIF_OPTIONS = {
+  // Desired width of the image
+  'gifWidth': 400,
+  // Desired height of the image
+  'gifHeight': 300,
+  // Whether or not the animated GIF text will be resized to fit within the GIF container
+'resizeFont': true,
+  // Note: Each frame is captured every 100 milleseconds of a video and every ms for existing images
+  'numFrames': GIF_DURATION / 100,
+}
+
+const ProgressBar = props => {
+  const myStyles = { transform: `scaleX(${props.percentage})`} 
+  return <div className="bar-wrapper">
+    <div className="bar" style={myStyles}></div>
+  </div>
+}
+
+class ProgressMapper extends Component {
+
+  state = { value: 0 }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.status === 'shoot') {
+        const interval = setInterval(() => {
+          const newValue = (this.state.value - 0.1).toFixed(1)
+          this.setState({ value: newValue })
+          if (this.state.value < 0){
+            clearInterval(interval)
+          }
+        }, 100)
+    }
+    this.setState({ value: GIF_DURATION / 1000 })
+  }
+
+  render() {
+    if (this.props.status === 'shoot') {
+      if (this.state.value < 0) {
+        return <Spinner name="ball-pulse-sync" fadeIn="none" className="processing" />
+      }
+      return <ProgressBar percentage={1 - this.state.value / (GIF_DURATION / 1000)} />
+    }
+    return null
+  }
+}
+
+class Giffer extends Component {
+
+  state = { stream: null, status: '' }
+
+  componentDidMount() {
+    this.startVideo()
+  }
+
+  render() {
+    const { status } = this.state
+    const previewWrapperStyles = classname('preview-wrapper', { 'preview-wrapper-active': status === 'preview'  })
+    const captureWrapperStyles = classname('capture-wrapper', { 'capture-wrapper-active': status !== 'preview' })
+    const shootBtnStyles = classname('shoot giffer-btns', { 'giffer-btns-active': status === 'capture' })
+    const acceptBtnStyles = classname('accept giffer-btns', { 'giffer-btns-active': status === 'preview'})
+    const againBtnStyles = classname('again giffer-btns', { 'giffer-btns-active': status === 'preview'})
+    return <div className="wrapper">
+      <div className="giffer-content">
+        <div className={previewWrapperStyles}>
+          <div className="preview">
+            <img alt="" ref="preview" />
+          </div>  
+        </div>
+        <div className={captureWrapperStyles}>
+          <div className="input">
+            <video ref="video" className="video" autoPlay></video>
+          </div> 
+          <div className="progress-wrapper">
+            <ProgressMapper status={status} />
+          </div>
+        </div>
+      </div>
+      <div className="giffer-controls">
+        <div className={acceptBtnStyles} onClick={this.accept}>
+          <img src={acceptIcon} className="accept-icon" />
+        </div>
+        <div className={againBtnStyles} onClick={this.again}>
+          <img src={againIcon} className="again-icon" />
+        </div>
+        <div className={shootBtnStyles} onClick={this.capture}>
+          <img src={shootIcon} className="shoot-icon" />
+        </div>
+        <div className="cancel giffer-btns giffer-btns-active" onClick={this.props.onCancel}>
+          <img src={cancelIcon} className="cancel-icon" />
+        </div>
+      </div>
+    </div>
+  }
+
+  startVideo = () => {
+    if (this.state.status !== 'capture') {
+      const video = this.refs.video
+      navigator.mediaDevices.getUserMedia({ video: true })
+        .then(stream => {
+          video.src = window.URL.createObjectURL(stream)
+          video.play()
+          this.setState({ stream, status: 'capture' })
+        })
+    }
+  }
+  
+  stopVideo = () => {
+    const video = this.refs.video
+    video.pause()
+    video.src = ''
+    this.state.stream.getTracks().forEach(t => t.stop())
+  }
+
+  accept = () => {
+    // todo
+    // I heard this urban legend of how instagram started uploading the user
+    // pics while they presented the user with the filters section, this way
+    // the whole process took less time (asynch upload), maybe we can do
+    // something similar. I mean, while we wait for the user to accept the GIF
+    // we could already be uploading it - although in reality we do not upload
+    // anything, but send it to the service workers to save to local storage.
+    // Or not and we just do syncrhonous process.
+  }
+  
+  again = () => {
+    this.removePreview()
+    this.startVideo()
+  }
+  
+  removePreview = () => {
+    this.refs.preview.src = ''
+  }
+
+  capture = () => {
+    this.setState({ status: 'shoot' }, () => {
+      gifshot.createGIF(GIF_OPTIONS, obj => {
+        if (obj.error) {
+          console.warn('GIFshot error: ', obj.error, obj.errorCode, obj.errorMsg, obj)
+          this.setStatus({ status: 'capture' })
+          return
+        }
+        this.stopVideo()
+        this.refs.preview.src = obj.image
+        this.setState({ status: 'preview' })
+      })
+    })
+  }
+}
+
+export default Giffer
