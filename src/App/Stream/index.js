b20f4cf536c657ab06cb3164cbcb5e9be4d7517c
Gorka <gorka@aquigorka.com>
2017-10-02 13:01:24 +0200

    Add some styles to make the stream look more simetrical and with margins
    Remove unnecesary brackets

--- src/App/Stream/index.js
+++ src/App/Stream/index.js
@@ -28,10 +28,11 @@ class Stream extends Component {
       return <p style={{marginLeft: 15}}>Make a GIF by clicking the camera icon below!</p>
     }
 
-    return order.map(id =>
-      <img key={id} alt="" src={gifs[id].src} className="stream__publication"/>
-    )
-
+    return <div className="stream__wrapper">
+        {order.map(id =>
+          <img key={id} alt="" src={gifs[id].src} className="stream__publication"/>
+        )}
+      </div>
   }
 }
 
--- src/database/localStorage.js
+++ src/database/localStorage.js
@@ -1,15 +1,15 @@
 import localforage from 'localforage'
 
-export const loadState = (key) => {
-  return localforage.getItem(`${key}`)
+
+export const loadState = (key) =>
+  localforage.getItem(`${key}`)
     .then(res => res || undefined)
     .catch(err => {
       console.warn(`There was an error retrieving item (${key}) from localstorage: ${err}`)
       return undefined
     })
-}
 
-export const saveState = (key, state) => {
-  return localforage.setItem(`${key}`, state)
-    .catch(err => console.warn(`There was an error setting item (${key}) to localstorage ${err}`))
-}
+export const saveState = (key, state) =>
+  localforage.setItem(`${key}`, state)
+    .catch(err =>
+      console.warn(`There was an error setting item (${key}) to localstorage ${err}`))
