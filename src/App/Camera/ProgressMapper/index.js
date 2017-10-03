808e02b6d5d6c5a72faaa590b15c51a993ba8970
Gorka <gorka@aquigorka.com>
2017-10-03 10:02:08 +0200

    ProgressMapper: remove unnecesary math computations on evey loop

--- src/App/Camera/ProgressMapper/index.js
+++ src/App/Camera/ProgressMapper/index.js
@@ -5,11 +5,11 @@ import { GIF_DURATION } from '../settings'
 
 export default class ProgressMapper extends React.Component {
 
-  state = { value: GIF_DURATION / 1000 }
+  state = { value: GIF_DURATION }
 
   componentDidMount() {
     const interval = setInterval(() => {
-      this.setState({ value: this.state.value - 0.1 })
+      this.setState({ value: this.state.value - 100 })
       if (this.state.value < 0) {
         clearInterval(interval)
       }
@@ -19,7 +19,7 @@ export default class ProgressMapper extends React.Component {
   render() {
     if (this.state.value < 0) return <Spinner />
 
-    return <ProgressBar percentage={1 - this.state.value / (GIF_DURATION / 1000)} />
+    return <ProgressBar percentage={ 1 - this.state.value / GIF_DURATION } />
   }
 }
 
