FormulaMonks/thicket

commit 4bf78263002726b8e4a7a6eca1b2b784bca7d342

Stop video during GIF processing
The see-yourself video now stops after the recording finishes.

src/App/Camera/index.js
@@ -8,7 +8,7 @@ import {
   Shoot as ShootIcon,
   Cancel as CancelIcon,
 } from '../Icons'
-import { GIF_OPTIONS } from './settings'
+import { GIF_DURATION, GIF_OPTIONS } from './settings'
 
 import { saveImage } from '../syncedDB'
 
@@ -77,8 +77,9 @@ class Giffer extends Component {
   }
 
   again = () => {
+    this.startVideo()
     this.removePreview()
-    this.setState({ mode: STANDBY}, this.capture)
+    this.setState({ mode: STANDBY})
   }
 
   removePreview = () => {
@@ -86,6 +87,7 @@ class Giffer extends Component {
   }
 
   capture = () => {
+    setTimeout(this.stopVideo, GIF_DURATION);
     this.setState({ mode: SHOOTING }, () => {
       gifshot.createGIF({
         ...GIF_OPTIONS,

src/App/Camera/settings.js
@@ -5,8 +5,6 @@ export const GIF_OPTIONS = {
   'gifWidth': 400,
   // Desired height of the image
   'gifHeight': 300,
-  // Whether or not the animated GIF text will be resized to fit within the GIF container
-  'resizeFont': true,
   // Note: Each frame is captured every 100 milleseconds of a video and every ms for existing images
   'numFrames': GIF_DURATION / 100,
 }

commit d38de48f4cf93ce798f296f9999d9faa3b6cbb58

Fix pause/blib at end of GIFs
Fixes issue introduced by 4bf78263002726b8e4a7a6eca1b2b784bca7d342

The camera getting turned off was happening a little too soon, and
affecting the GIF.

src/App/Camera/index.js
@@ -88,7 +88,7 @@ class Camera extends Component {
   }
 
   capture = () => {
-    setTimeout(this.stopVideo, GIF_DURATION);
+    setTimeout(this.stopVideo, GIF_DURATION + 500);
     this.setState({ mode: SHOOTING }, () => {
       gifshot.createGIF({
         ...GIF_OPTIONS,