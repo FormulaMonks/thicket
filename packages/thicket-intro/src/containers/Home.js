284a5ed3cf3a43619afff770dfac318ca627bd5e
Chad Ostrowski <hi@chadoh.com>
2017-10-10 16:09:25 -0400

    intro: rm unneeded routes, packages, & components

--- packages/thicket-intro/src/containers/Home.js
+++
@@ -1,3 +0,0 @@
-import React from 'react'
-//
-export default () => <h1>Welcome Home!</h1>
--- packages/thicket-intro/static.config.js
+++ packages/thicket-intro/static.config.js
@@ -1,31 +1,12 @@
-import axios from 'axios'
 import React, { Component } from 'react'
 import { ServerStyleSheet } from 'styled-components'
 
 export default {
-  getRoutes: async () => {
-    const { data: posts } = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    return [
-      {
-        path: '/',
-      },
-      {
-        path: '/about',
-      },
-      {
-        path: '/blog',
-        getProps: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          getProps: () => ({
-            post,
-          }),
-        })),
-      },
-    ]
-  },
+  getRoutes: () => [
+    {
+      path: '/',
+    },
+  ],
   Html: class CustomHtml extends Component {
     render () {
       const { Html, Head, Body, children } = this.props
