54b9e3e958ccf4ebf67fba913e9bb681d370aed6
Chad Ostrowski <hi@chadoh.com>
2017-10-10 20:26:02 -0400

    Intro: add meta tags

--- packages/thicket-intro/static.config.js
+++ packages/thicket-intro/static.config.js
@@ -15,9 +15,22 @@ export default {
       const newChildren = sheet.collectStyles(children)
       const styleTags = sheet.getStyleElement()
 
+      const title = 'This is Thicket, an Offline First app built for the most extreme network conditions'
+      const description = "Citrusbyte can build new types of apps that don't require any central servers. Learn about one called Thicket."
+
       return (
         <Html>
           <Head>
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="og:title" content={title} />
+            <meta name="og:description" content={description} />
+
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:creator" content="@citrusbyte" />
+            <meta name="twitter:title" content={title} />
+            <meta name="twitter:description" content={description} />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             {styleTags}
           </Head>
