4299977fa9bbfe5481a38adf024ac93df07c6efa
Gorka <gorka@aquigorka.com>
2017-10-16 12:07:36 +0200

    Remove local scripts and dependencies

--- packages/thicket-webapp/scripts/deploy.sh
+++
@@ -1,3 +0,0 @@
-yarn build
-mv ./build/index.html ./build/200.html
-node_modules/.bin/surge ./build https://thicket.surge.sh
--- packages/thicket-webapp/scripts/hack-source.sh
+++
@@ -1,7 +0,0 @@
-hack_source() {
-  file="./node_modules/$1"
-  echo "babelify $file"
-  ./node_modules/babel-cli/bin/babel.js $file --presets=es2015 -o $file
-  echo "uglify $file"
-  ./node_modules/uglify-es/bin/uglifyjs $file -o $file
-}
