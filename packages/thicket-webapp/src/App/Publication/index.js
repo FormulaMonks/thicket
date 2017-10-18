6549a98826fd30fd7f9ea3c723e8e3300aca4f68
Gorka <gorka@aquigorka.com>
2017-10-18 11:50:44 +0200

    Social share: add facebook and twitter share links

--- packages/thicket-webapp/src/App/Publication/index.js
+++ packages/thicket-webapp/src/App/Publication/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { deleteImage } from '../syncedDB'
 import { Cancel as DeleteIcon } from '../NavLinks'
 import './Publication.css'
+import { homepage } from '../../../package.json'
+
+const twitterURL = 'https://twitter.com/intent/tweet'
+const facebookURL = 'https://www.facebook.com/sharer.php'
 
 const del = (history, id) => () => {
   deleteImage(id)
@@ -11,6 +15,7 @@ const del = (history, id) => () => {
 export default ({publications, match, history}) => {
   const id = match.params.id
   const gif = publications[id]
+  const url = `${homepage}/${id}`
 
   return <div className="publication">
     <div className="publication__content">
@@ -20,8 +25,13 @@ export default ({publications, match, history}) => {
         alt=""
       />
     </div>
+    <div>
+      <a href={`${twitterURL}?url=${url}`} target="_blank">tweet this</a>
+      <a href={`${facebookURL}?u=${url}`} target="_blank">post to facebook</a>
+    </div>
     <div className="publication__controls">
       <DeleteIcon onClick={del(history, id)} alt="Delete GIF" />
     </div>
   </div>
 }
+
