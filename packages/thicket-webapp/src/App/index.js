033e2c547425499040e34f13edbccf8ecdf3c99a
Gorka <gorka@aquigorka.com>
2017-10-27 14:52:58 +0200

    Add persistent communities

--- packages/thicket-webapp/src/App/index.js
+++ packages/thicket-webapp/src/App/index.js
@@ -11,17 +11,19 @@ import { BottomNav as Nav } from 'thicket-elements'
 import BackNav from './Back'
 import { Spinner } from 'thicket-elements'
 import './App.css'
+import { set, get } from '../database/localStorage'
 
 import db, { initialState } from './syncedDB';
 
 class App extends Component {
 
-  state = { ...initialState, loaded: false }
+  state = { ...initialState, loaded: false, communities: [] }
 
   componentDidMount() {
     db.addSaveSuccessListener(this.fetchData)
     db.addSaveFailListener(this.handleDatabaseError)
 
+    get('communities').then(data => this.setState({ communities: data || [] }))
     this.fetchData()
   }
 
@@ -55,10 +57,10 @@ class App extends Component {
             ? <div className="stream__spinner"><Spinner /></div>
             : <Switch>
                 <Route exact path="/" render={() => {
-                    /*<Stream {...data} />*/
                     return [
                       <div key="mine">My GIFs?</div>,
                       <div key="commis">My Communities?</div>,
+                      <Stream key="stream" {...data} />
                     ]
                   }
                 }/>
@@ -69,8 +71,8 @@ class App extends Component {
                 <Route exact path="/c/:c" render={props =>
                   <Community {...data} {...props}/>
                 }/>
-                <Route exact path="/invite/:hash" render={props =>
-                  <Invite {...props}/>
+                <Route exact path="/invite/:code" render={props =>
+                  <Invite {...props} communities={this.state.communities}/>
                 }/>
               </Switch>
           }
@@ -86,31 +88,29 @@ class App extends Component {
 }
 
 const Invite = props => {
+  const { communities } = props
   let i = ''
   let redirect = '/'
   try {
-    i = atob(props.match.params.hash)
+    i = atob(props.match.params.code)
   } catch(e) {
-    console.log('No invite for you with that code')
+    console.log('No invite for you with this code')
   }
   const d = new Date(parseInt(i.substr(0,13),10))
   const t = new Date()
   if ((t.getTime() - d.getTime()) < 24 * 60 * 60 * 1000) {
     const c = atob(i.substr(13))
     console.log(`Congratulations, you got your invite to ${c}`)
-    redirect = `/c/${c}`
+    set('communities', communities.filter(x => x !== c).concat(c))
+      .then(() => props.history.push(`/c/${c}`))
   }
-  props.history.push(`/`)
-  setImmediate(() => props.history.push(redirect))
-  return null
+  return <div>This code is not valid</div>
 }
-
-const myC = ['cats']
 
 const Community = props => {
   const c = props.match.params.c
   const { publications } = props
-  if (c.indexOf('public-') === 0 || myC.includes(c)) {
+  if (c.indexOf('public-') === 0 || props.communities.includes(c)) {
     const filtered = Object.keys(publications)
       .filter(x => publications[x].tags && publications[x].tags.includes(c))
       .map(x => publications[x])
