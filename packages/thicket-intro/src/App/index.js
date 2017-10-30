b9787908ede9a35384b3cd4d63cbc99b4d078e48
Chad Ostrowski <hi@chadoh.com>
2017-10-30 18:24:55 -0400

    Intro: Fill in "Motivation" section
    Partially fulfills #74
    
    This adds a short blurb to this section, replete with links to some of
    my favorite articles on the subject. It also adds the two images that
    @s-small created, which look :100:

--- packages/thicket-intro/src/App/index.js
+++ packages/thicket-intro/src/App/index.js
@@ -2,13 +2,14 @@ import React from 'react'
 //
 import Hero from './Hero'
 import GifCreator from './GifCreator'
+import Why from './Why'
 import Filler from './Filler'
 import './App.css'
 
 export default () => [
   <Hero key="hero" scrollTo="#create" />,
-  <GifCreator key="create" id="create" scrollTo="#motivation" />,
-  <Filler key="1" title="Motivation &amp; Why We Built It" id="motivation" />,
+  <GifCreator key="create" id="create" scrollTo="#why" />,
+  <Why key="why" id="why" />,
   <Filler key="2" title="How’s Thicket work?" />,
   <Filler key="3" title="Open Source &amp; More Info" />,
   // <ShareYourGif key="share" />,
