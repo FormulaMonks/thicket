029f079de6d914cd508f7381789c702ce39a41fc
Chad Ostrowski <hi@chadoh.com>
2017-10-31 11:39:38 -0400

    Intro/What: Add better images to links

--- packages/thicket-intro/src/App/What/index.js
+++ packages/thicket-intro/src/App/What/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import Columns from '../Columns'
 import Square from '../Square'
-import placeholder from './placeholder.svg'
+import onlineOfflineEquivalence from './onlineOfflineEquivalence.jpg'
+import stateOnFrontend from './stateOnFrontend.png'
+import thicket from './thicket.png'
 
 import './What.css'
 
@@ -15,16 +17,16 @@ export default () => (
         our blog posts.
       </p>
       <Columns>
-        <a className="muted" href="https://www.youtube.com/watch?v=FXhPBiv4Roo&index=24&list=PLe9psSNJBf743rgLMRVKytyQkDUolnZnY&t=1s">
-          <Square background={placeholder} />
+        <a className="muted" href="https://www.youtube.com/watch?v=FXhPBiv4Roo">
+          <Square background={onlineOfflineEquivalence} />
           <div><span className="highlight">Watch:</span> What Our Interplanetary Tomorrow Can Teach Us About Building Apps Today</div>
         </a>
         <a className="muted" href="https://medium.com/@denis.sokolov_53985/state-on-the-front-end-5b80c0045f07">
-          <Square background={placeholder} />
+          <Square background={stateOnFrontend} />
           <div><span className="highlight">Read:</span> State on the front-end</div>
         </a>
         <a className="muted" href="https://github.com/citrusbyte/thicket">
-          <Square background={placeholder} />
+          <Square background={thicket} />
           <div><span className="highlight">Code:</span> Thicket source code</div>
         </a>
       </Columns>
