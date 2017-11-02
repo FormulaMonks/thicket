5e3f59d4395cb5fe352da1cd87e92cfa1fa6ff37
Gorka <gorka@aquigorka.com>
2017-11-02 15:17:18 +0100

    Review: extract common component

--- packages/thicket-camera/src/Camera/Review/index.js
+++ packages/thicket-camera/src/Camera/Review/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Button } from 'thicket-elements'
 
 const Preview = styled.img`
   flex: 1;
@@ -11,19 +12,6 @@ const Controls = styled.div`
   width: 100%;
   display: flex;
   margin: 10px 0 0 0;
-`
-const Button = styled.button`
-  cursor: pointer;
-  border: 1px solid #000;
-  padding: 20px;
-  background: #FFF;
-  color: #000;
-  width: 50%;
-  
-  &:hover{
-    background: #000;
-    color: #FFF;
-  }
 `
 
 const Review = props => {
