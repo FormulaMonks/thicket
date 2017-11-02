75edeabf7889619439c85e865aec1ea58f371265
Gorka <gorka@aquigorka.com>
2017-11-02 10:49:41 +0100

    Demo: add styles setup

--- packages/thicket-camera/demo/src/index.js
+++ packages/thicket-camera/demo/src/index.js
@@ -2,15 +2,14 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import Camera from '../../src'
+import './styles.css'
 
 class Demo extends Component {
   render() {
-    return <div>
-      <h1>thicket-camera Demo</h1>
-      <div style={{ width: 600, height: 600 }}>
-        <Camera onSave={() => alert('saving yr gif!')}/>
-      </div>
-    </div>
+    return [
+      <h1>thicket-camera Demo</h1>,
+      <Camera onSave={() => alert('saving yr gif!')}/>
+    ]
   }
 }
 
