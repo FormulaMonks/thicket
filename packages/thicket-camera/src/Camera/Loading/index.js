94f32507f00d36491beb4c755de3b91578b91850
Gorka <gorka@aquigorka.com>
2017-11-06 09:21:55 +0100

    Loading: use thicket-elements spinner

--- packages/thicket-camera/src/Camera/Loading/index.js
+++ packages/thicket-camera/src/Camera/Loading/index.js
@@ -1,17 +1,6 @@
 import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import { Spinner } from 'thicket-elements'
 
-const spin = keyframes`
-  to {transform: rotate(360deg);}
-`
-const Spinner = styled.div`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  border-top: 3px solid #000;
-  border-right: 2px solid transparent;
-  animation: ${spin} 1s linear infinite;
-`
 const Loading = props => {
   const { loadingSpinner, loadingTitle, loadingMessage } = props.classNames
   return [
