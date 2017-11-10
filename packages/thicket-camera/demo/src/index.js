8e497172d9c3e3f3b2bfacf98db47039eef5d29c
Chad Ostrowski <hi@chadoh.com>
2017-11-10 10:40:21 -0500

    Camera: Remove all demo styles
    I want the demo to show the raw component, as it would appear in my app
    if I don't add anything extra.

--- packages/thicket-camera/demo/src/index.js
+++ packages/thicket-camera/demo/src/index.js
@@ -2,14 +2,13 @@ import React, {Component} from 'react'
 import {render} from 'react-dom'
 
 import Camera from '../../src'
-import './styles.css'
 
 class Demo extends Component {
   render() {
-    return [
-      <h1 key="title">thicket-camera Demo</h1>,
-      <Camera key="camera" onSave={() => alert('saving yr gif!')} />
-    ]
+    return <div>
+      <h1>thicket-camera Demo</h1>
+      <Camera onSave={() => alert('saving yr gif!')} />
+    </div>
   }
 }
 
