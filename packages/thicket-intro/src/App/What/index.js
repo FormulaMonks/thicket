584e5943803bcdb10928bec4704044e8b38019ef
Chad Ostrowski <hi@chadoh.com>
2017-11-15 12:20:13 -0500

    Intro/What: Make image height consistent
    Trim all images used in this section to 400x225px, and add other
    supporting styles suggested by Sam in #87

--- packages/thicket-intro/src/App/What/index.js
+++ packages/thicket-intro/src/App/What/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Columns from '../Columns'
-import Square from '../Square'
 import onlineOfflineEquivalence from './onlineOfflineEquivalence.jpg'
 import stateOnFrontend from './stateOnFrontend.png'
 import thicket from './thicket.png'
@@ -16,18 +15,18 @@ export default () => (
         were already exploring. Check out our code, our conference talks, and
         our blog posts.
       </p>
-      <Columns>
+      <Columns style={{textAlign: 'left'}}>
         <a className="muted" href="https://www.youtube.com/watch?v=FXhPBiv4Roo">
-          <Square background={onlineOfflineEquivalence} />
-          <div><span className="highlight">Watch:</span> What Our Interplanetary Tomorrow Can Teach Us About Building Apps Today</div>
+          <img alt="" src={onlineOfflineEquivalence} />
+          <div><div className="highlight">Watch:</div> What Our Interplanetary Tomorrow Can Teach Us About Building Apps Today</div>
         </a>
         <a className="muted" href="https://medium.com/@denis.sokolov_53985/state-on-the-front-end-5b80c0045f07">
-          <Square background={stateOnFrontend} />
-          <div><span className="highlight">Read:</span> State on the front-end</div>
+          <img alt="" src={stateOnFrontend} />
+          <div><div className="highlight">Read:</div> State on the front-end</div>
         </a>
         <a className="muted" href="https://github.com/citrusbyte/thicket">
-          <Square background={thicket} />
-          <div><span className="highlight">Code:</span> Thicket source code</div>
+          <img alt="" src={thicket} />
+          <div><div className="highlight">Code:</div> Thicket source code</div>
         </a>
       </Columns>
     </div>
