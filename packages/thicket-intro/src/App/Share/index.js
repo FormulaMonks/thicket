eafb2a08360d9e8fbc8516b1a3223f4341ead54c
Chad Ostrowski <hi@chadoh.com>
2017-11-21 16:46:41 -0500

    Intro: make sharing buttons actually do something
    The Facebook and Twitter share links work well. The Link button is not
    beautiful yet; I am waiting on designs from Sam for that.

--- packages/thicket-intro/src/App/Share/index.js
+++ packages/thicket-intro/src/App/Share/index.js
@@ -7,26 +7,61 @@ import facebook from './facebook.svg'
 import twitter from './twitter.svg'
 import './Share.css'
 
-export default () => (
-  <Section
-    style={{
-      backgroundColor: '#AC3434',
-      paddingBottom: '1em',
-    }}
-  >
-    <p>
-      Share this page with others:
-    </p>
-    <div className="Share-buttons">
-      <button onClick={() => alert('todo')}>
-        <img alt="copy link" src={link} />
-      </button>
-      <button onClick={() => alert('todo')}>
-        <img alt="share on facebook" src={facebook} />
-      </button>
-      <button onClick={() => alert('todo')}>
-        <img alt="tweet" src={twitter} />
-      </button>
-    </div>
-  </Section>
-)
+const pageURL = window.encodeURIComponent(window.location.href)
+
+export default class Share extends React.Component {
+
+  state = { linking: false }
+
+  maybeHideDropdown = e => {
+    if (
+      !e.relatedTarget ||
+      (this.sharebox && !this.sharebox.contains(e.relatedTarget))
+    ) {
+      this.setState({ linking: false })
+    }
+  }
+
+  render() {
+    return (
+      <Section
+        style={{
+          backgroundColor: '#AC3434',
+          paddingBottom: '1em',
+        }}
+      >
+        <p>
+          Share this page with others:
+        </p>
+        <div
+          className="Share-buttons"
+          tabIndex="0"
+          onBlur={this.maybeHideDropdown}
+          ref={div => this.sharebox = div}
+        >
+          <button className="Share-button" onClick={() => this.setState({ linking: true })}>
+            <img alt="copy link" src={link} />
+          </button>
+          <a className="Share-button" target="_blank" href={`https://www.facebook.com/sharer/sharer.php?u=${pageURL}`}>
+            <img alt="share on facebook" src={facebook} />
+          </a>
+          <a className="Share-button" target="_blank" href={`https://twitter.com/home?status=Mars.%20GIFs.%20Fun%20times.%20Check%20it%20out%3A%20${pageURL}`}>
+            <img alt="tweet" src={twitter} />
+          </a>
+          {this.state.linking &&
+              <div className="Share-link">
+                <label>
+                  Copy the link and share it with your friends:
+                  <input
+                    readOnly
+                    value={window.location.href}
+                    ref={input => { if (input) { input.focus(); input.select(); } } }
+                  />
+                </label>
+              </div>
+          }
+        </div>
+      </Section>
+    )
+  }
+}
