14d737b1f0482ae16608bf2743bcf9b36adb8c29
Chad Ostrowski <hi@chadoh.com>
2017-11-28 18:08:21 -0500

    Webapp: simplify onboarding wordigs logic
    Prep for adding better wording.

--- packages/thicket-webapp/src/components/QuickExplanation/index.js
+++ packages/thicket-webapp/src/components/QuickExplanation/index.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react'
 import { Button } from 'thicket-elements'
 
+const wordings = new Map([
+  [
+    1,
+    `Step 1`
+  ],
+  [
+    2,
+    `Step 2`
+  ],
+  [
+    3,
+    `Step 3`
+  ],
+])
+
 class QuickExplanation extends Component {
 
   state = { step: 1 }
@@ -11,15 +26,15 @@ class QuickExplanation extends Component {
 
     return <div className="onboarding">
       <div>Learn more about how Thicket works below.</div>
-      {step === 1 && <div onClick={() => this.setState({ step: 2 })} className="onboarding__step1">Step 1.</div>}
-      {step === 2 && <div onClick={() => this.setState({ step: 3 })} className="onboarding__step2">Step 2.</div>}
-      {step === 3 && <div onClick={() => this.setState({ step: 4 })} className="onboarding__step3">All done.</div>}
+      <div>
+        {wordings.get(step)}
+      </div>
       <div className="onboarding__controls">
-        {step !== 3 && [
+        {step !== wordings.size && [
           <Button onClick={onComplete} key="skip">Skip Onboarding</Button>,
           <Button onClick={() => this.setState({ step: this.state.step + 1 })} key="next">Next</Button>
         ]}
-        {step === 3 && <Button onClick={onComplete}>Finish</Button>}
+        {step === wordings.size && <Button onClick={onComplete}>Finish</Button>}
       </div>
     </div>
   }
