7f96e1973d98c22cc8ce69c36083530d67f3748e
Gorka <gorka@aquigorka.com>
2017-12-05 16:05:57 +0100

    Card: add fetch and render community data

--- packages/thicket-webapp/src/App/Communities/Card/index.js
+++ packages/thicket-webapp/src/App/Communities/Card/index.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react'
 import favorite from './favorite.svg'
 import './Card.css'
+import store from '../../../database/store'
+
+const { communities } = store
 
 class Card extends Component {
+
+  state = { title: '...', createdBy: '...', usage: '' }
+
+  async componentDidMount() {
+    const community = await communities.get(this.props.communityId)
+    const { title, createdBy } = await community.get()
+    this.setState({ title, createdBy: `Created by ${createdBy}` })
+  }
+
   render() {
+    const { title, createdBy, usage } = this.state
+
     return <div className="card">
       <img src={favorite} alt="" className="card__favorite" />
       <div className="card__meta">
-        <div className="card__title">Community Title</div>
+        <div>{title}</div>
         <div className="card__extra">
-          <div>Created by username</div>
-          <div className="card__usage">disk usage</div>
+          <div>{createdBy}</div>
+          <small>{usage}</small>
         </div>
       </div>
     </div>
