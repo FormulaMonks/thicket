16a2e5b091470e2c9c1bd5fb576aaa8b315ba70a
Chad Ostrowski <hi@chadoh.com>
2017-12-06 16:59:28 -0500

    Button: swap active & hover states; remove outline
    Minor tweaks to the style of the button. Sam-approved.

--- packages/thicket-elements/src/sharedStyles.js
+++ packages/thicket-elements/src/sharedStyles.js
@@ -1,4 +1,5 @@
 export const defaultBoxShadow = '0px 2px 2px rgba(113, 114, 215, 0.34), 0px 0px 2px rgba(113, 114, 215, 0.22)'
-export const activeBoxShadow =  '0px 0px 8px rgba(113,114,215,0.32), inset 0 0 0 99em rgba(255,255,255,0.05)'
+export const hoverBoxShadow = `${defaultBoxShadow}, inset 0 0 0 99em rgba(255,255,255,0.07)`
+export const activeBoxShadow =  '0px 0px 8px rgba(113,114,215,0.32), inset 0 0 0 99em rgba(0,0,0,0.1)'
 export const linearGradient = 'linear-gradient(23deg, #F7618B, #2A7AFF 140%)'
 export const radialGradient = 'radial-gradient(circle at top left,#F7618B, #2A7AFF)'
