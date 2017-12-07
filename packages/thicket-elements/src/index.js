4ff87a3d41acf601ddccc538641d5f7795592365
Gorka <gorka@aquigorka.com>
2017-12-07 14:30:40 +0100

    Modal: add modal to thicket elements

--- packages/thicket-elements/src/index.js
+++ packages/thicket-elements/src/index.js
@@ -34,6 +34,14 @@ export default () => (
         <p>Tempus iaculis urna id volutpat lacus laoreet. Malesuada bibendum arcu vitae elementum curabitur. Amet risus nullam eget felis eget. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Ac auctor augue mauris augue neque. Et egestas quis ipsum suspendisse. Cras tincidunt lobortis feugiat vivamus at. Aenean vel elit scelerisque mauris pellentesque pulvinar. Id diam vel quam elementum pulvinar etiam non. Sagittis purus sit amet volutpat. Suspendisse faucibus interdum posuere lorem ipsum dolor sit. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Purus sit amet luctus venenatis. Lobortis scelerisque fermentum dui faucibus in. Enim nec dui nunc mattis enim. Senectus et netus et malesuada fames ac turpis. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Lectus nulla at volutpat diam ut venenatis tellus.</p>
       </Modal>
     </div>
+    <h2>Modal</h2>
+    <div style={{ height: 600, width: '100%', background: '#FFF', overflow: 'auto', boxSizing: 'border-box', border: '2px dashed #000', position: 'relative' }}>
+      <Modal>
+        <h3>What is Lorem Ipsum?</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Placerat in egestas erat imperdiet sed. In nibh mauris cursus mattis. Convallis convallis tellus id interdum velit laoreet. Neque viverra justo nec ultrices. Egestas pretium aenean pharetra magna ac. At tempor commodo ullamcorper a lacus vestibulum sed. Scelerisque purus semper eget duis at. Id ornare arcu odio ut. Donec massa sapien faucibus et molestie ac feugiat sed. Massa id neque aliquam vestibulum morbi blandit cursus risus.</p>
+        <p>Tempus iaculis urna id volutpat lacus laoreet. Malesuada bibendum arcu vitae elementum curabitur. Amet risus nullam eget felis eget. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Ac auctor augue mauris augue neque. Et egestas quis ipsum suspendisse. Cras tincidunt lobortis feugiat vivamus at. Aenean vel elit scelerisque mauris pellentesque pulvinar. Id diam vel quam elementum pulvinar etiam non. Sagittis purus sit amet volutpat. Suspendisse faucibus interdum posuere lorem ipsum dolor sit. Amet consectetur adipiscing elit pellentesque habitant morbi tristique senectus. Purus sit amet luctus venenatis. Lobortis scelerisque fermentum dui faucibus in. Enim nec dui nunc mattis enim. Senectus et netus et malesuada fames ac turpis. Elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi tristique senectus. Lectus nulla at volutpat diam ut venenatis tellus.</p>
+      </Modal>
+    </div>
   </div>
 )
 
