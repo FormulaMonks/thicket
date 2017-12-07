76d98728b5bfdd91ac2d84a47543d93a1624b639
Chad Ostrowski <hi@chadoh.com>
2017-12-07 11:50:12 -0500

    Secondary Button: Roll with the inherit hack
    This documents what's needed in the code that uses `<Button secondary>`,
    right in the demo page for `thicket-elements`.

--- packages/thicket-elements/src/index.js
+++ packages/thicket-elements/src/index.js
@@ -6,13 +6,18 @@ import Input from './Input'
 import * as Styles from './sharedStyles'
 
 export default () => (
-  <div style={{backgroundColor: 'inherit'}}>
+  <div>
     <h2>Spinner</h2>
     <Spinner />
     <h2>Button</h2>
     <Button onClick={() => console.log('button!')}>Click Me</Button>
     <h2>Secondary Button</h2>
     <Button secondary onClick={() => console.log('button!')}>Click Me</Button>
+    <p>
+      <small>
+        <code>SecondaryButton</code> needs a global <code>* &#123; background-color: inherit &#125;</code> to work properly
+      </small>
+    </p>
     <h2>Text Input</h2>
     <Input placeholder="Text Input" />
   </div>
