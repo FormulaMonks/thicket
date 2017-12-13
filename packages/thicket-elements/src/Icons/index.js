a4ac58151176a09ea2bd9b2b35c1a3576f4c8ef6
Gorka <gorka@aquigorka.com>
2017-12-13 18:26:57 +0100

    Thicket elements: add icon & icons

---
+++ packages/thicket-elements/src/Icons/index.js
@@ -0,0 +1,4 @@
+export { default as downloadSvg } from './download.svg'
+export { default as shareSvg } from './share.svg'
+export { default as facebookSvg } from './facebook.svg'
+export { default as twitterSvg } from './twitter.svg'
