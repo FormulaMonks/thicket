3ec7d610b868636805589e28e613d390f03ea831
Gorka <gorka@aquigorka.com>
2018-01-11 17:02:22 +0100

    Modal: fix z index

--- packages/thicket-elements/src/Modal/index.js
+++ packages/thicket-elements/src/Modal/index.js
@@ -17,7 +17,7 @@ const Background = styled.div`
     left: 0;
     right: 0;
     bottom: 0;
-    z-index: 2;
+    z-index: 12;
     display: flex;
     align-items: center;
     justify-content: center;
