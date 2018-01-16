14d0b928274320d2fcd28f100f3ce10ff602ca69
Gorka <gorka@aquigorka.com>
2018-01-16 15:47:32 +0100

    Username: remove duplicate code into reusable component

--- packages/thicket-webapp/src/App/UserProfile/index.js
+++ packages/thicket-webapp/src/App/UserProfile/index.js
@@ -1,18 +1,10 @@
 import React from 'react'
-import randomColor from 'randomcolor'
+import UserName from '../../components/UserName'
 import editSvg from './edit.svg'
 import './UserProfile.css'
 
 export default ({ nickname }) => (
   <div className="userProfile">
-    <span
-      className="userProfile__initial"
-      style={{
-        background: randomColor({ seed: nickname, luminosity: 'dark' }),
-      }}
-    >
-      {nickname.substr(0, 1)}
-    </span>{' '}
-    {nickname} <img src={editSvg} alt="Edit profile" />
+    <UserName str={nickname} /> <img src={editSvg} alt="Edit profile" />
   </div>
 )
