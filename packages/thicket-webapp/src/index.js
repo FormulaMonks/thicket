7fb5d5057e078d6e3a00cc5cd9debe3a5e9348c5
Gorka <gorka@aquigorka.com>
2018-01-30 09:53:26 +0100

    Remove welcome and mapper components

--- packages/thicket-webapp/src/index.js
+++ packages/thicket-webapp/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Mapper from './Mapper';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Mapper />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
