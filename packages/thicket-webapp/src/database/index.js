dd20a8a2f47cc13ee17a61b1dd43090ca8af838f
Gorka <gorka@aquigorka.com>
2018-02-02 11:30:01 +0100

    Modify timeout to give more time for apps to download gif data

--- packages/thicket-webapp/src/database/index.js
+++ packages/thicket-webapp/src/database/index.js
@@ -51,7 +51,7 @@ const toBase64 = src =>
 // read more here
 // https://github.com/ipfs/js-ipfs/issues/800#issuecomment-290988388
 const timedSrcCat = async (node, id) => Promise.race([
-  new Promise(r => setTimeout(() => r(`https://ipfs.io/ipfs/${id}`), 3000)),
+  new Promise(r => setTimeout(() => r(`https://ipfs.io/ipfs/${id}`), 10000)),
   new Promise(async r => {
     const stream = await node.files.cat(id)
     r(toBase64(stream))
