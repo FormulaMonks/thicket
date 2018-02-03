5e370dda3ba68a6dc687da8c8f16c2f985475d5d
Gorka <gorka@aquigorka.com>
2018-02-03 11:15:24 +0100

    Gif: add share link modal

--- packages/thicket-webapp/src/components/Gif/index.js
+++ packages/thicket-webapp/src/components/Gif/index.js
@@ -1,9 +1,11 @@
-import React from 'react'
-import { Icons, Icon, Styles, Spinner } from 'thicket-elements'
+import React, { Component } from 'react'
+import { Icons, Icon, Styles, Spinner, ShareLink } from 'thicket-elements'
+import { Trigger } from 'thicket-elements/lib/ShareLink'
 import './Gif.css'
 import Editable from '../Editable'
 import streamSaver from 'streamsaver'
 import ImageDataConverter from '../../utils/imageDataConverter'
+import { getGIFLink } from '../../utils/links'
 
 const { linearGradient, glow } = Styles
 const { downloadSvg, shareSvg, facebookSvg, twitterSvg } = Icons
@@ -17,55 +19,87 @@ const downloadFile = gif => {
   writer.close()
 }
 
-export default ({
-  onShareHook=()=>{},
-  gif,
-  header,
-  editable,
-  onChange,
-  communityId,
-  children
-}) => {
-  if (!gif) {
-    return <div className="gif__loading"><Spinner /></div>
-  }
+export default class Gif extends Component {
 
-  const { src, nickname, caption } = gif
+  state = { sharing: false }
 
-  return <div className="gif__wrap" style={{ background: linearGradient, boxShadow: glow }}>
-    <img className="gif__img" src={src} alt={caption} />
-    <div className="gif__inner">
-      {header}
-      {(nickname || editable) && [
-        <h3 key="nickname_label">GIF created by:</h3>,
-        editable
-          ? <Editable key="nickname_input" value={nickname} onChange={e => onChange({ ...gif, nickname: e.currentTarget.value })} />
-          : <div key="nickname_value">{nickname}</div>
-      ]}
-      {(caption || editable) && [
-        <h3 key="caption_label">GIF caption:</h3>,
-        editable
-          ? <Editable key="caption_input" value={caption} onChange={e => onChange({ ...gif, caption: e.currentTarget.value })} />
-          : <div key="caption_value">{caption}</div>
-      ]}
-      <h3>Save & Share GIF</h3>
-      <div className="gif__buttons">
-        <button className="gif__button" onClick={() => downloadFile(gif)}>
-          <Icon src={downloadSvg} />
-        </button>
-        <button
-          className="gif__button"
-          onClick={() => {
-            onShareHook()
-            // share box
-          }}
-        >
-          <Icon src={shareSvg} />
-        </button>
-        <a href={`https://www.facebook.com/sharer.php?u=${window.location.href}`} target="_blank"><Icon src={facebookSvg} /></a>
-        <a href={`https://twitter.com/intent/tweet?url=${window.location.href}`} target="_blank"><Icon src={twitterSvg} /></a>
+  render() {
+    const {
+      onShareHook=cb=>cb(),
+      gif,
+      header,
+      editable,
+      onChange,
+      communityId,
+      children
+    } = this.props
+    const { sharing } = this.state
+
+    if (!gif) {
+      return <div className="gif__loading"><Spinner /></div>
+    }
+
+    const { src, nickname, caption, id } = gif
+    const publicURL = getGIFLink(communityId, id)
+
+    return <div className="gif__wrap" style={{ background: linearGradient, boxShadow: glow }}>
+      <img className="gif__img" src={src} alt={caption} />
+      <div className="gif__inner">
+        {header}
+        {(nickname || editable) && [
+          <h3 key="nickname_label">GIF created by:</h3>,
+          editable
+            ? <Editable key="nickname_input" value={nickname} onChange={e => onChange({ ...gif, nickname: e.currentTarget.value })} />
+            : <div key="nickname_value">{nickname}</div>
+        ]}
+        {(caption || editable) && [
+          <h3 key="caption_label">GIF caption:</h3>,
+          editable
+            ? <Editable key="caption_input" value={caption} onChange={e => onChange({ ...gif, caption: e.currentTarget.value })} />
+            : <div key="caption_value">{caption}</div>
+        ]}
+        <h3>Save & Share GIF</h3>
+        <div className="gif__buttons">
+          <button
+            className="gif__button"
+            onClick={() => {
+              if (onShareHook()) {
+                downloadFile(gif)
+              }
+            }}
+          >
+            <Icon src={downloadSvg} />
+          </button>
+          <Trigger active={sharing}>
+            <button
+              className="gif__button"
+              onClick={() => onShareHook(() => this.setState({ sharing: true }))}
+            >
+              <Icon src={shareSvg} />
+            </button>
+          </Trigger>
+          {sharing && <ShareLink
+            className="gif__share"
+            title={"GIF Share Link"}
+            body={"Share the link with friends so they view this GIF."}
+            toCopy={publicURL}
+            onBlur={() => this.setState({ sharing: false })}
+          />}
+          <button
+            className="gif__button"
+            onClick={() => onShareHook(() => window.open(`https://www.facebook.com/sharer.php?u=${publicURL}`, '_blank'))}
+          >
+            <Icon src={facebookSvg} />
+          </button>
+          <button
+            className="gif__button"
+            onClick={() => onShareHook(() => window.open(`https://www.twitter.com/intent/tweet?url=${publicURL}`, '_blank'))}
+          >
+            <Icon src={twitterSvg} />
+          </button>
+        </div>
+        {children}
       </div>
-      {children}
     </div>
-  </div>
+  }
 }
