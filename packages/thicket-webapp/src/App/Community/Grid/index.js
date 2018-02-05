18a59a77c702b0867261987e67111af27da29119
Gorka <gorka@aquigorka.com>
2018-02-05 10:16:06 +0100

    Grid: add styled spinner

--- packages/thicket-webapp/src/App/Community/Grid/index.js
+++ packages/thicket-webapp/src/App/Community/Grid/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Styles } from 'thicket-elements'
+import { Styles, Spinner } from 'thicket-elements'
 import prettydate from 'pretty-date'
 import { PLACEHOLDER } from '../../../utils/constants'
 import './Grid.css'
@@ -11,7 +11,18 @@ const Grid = ({ community, list }) => <ul className="communityGrid">
   {list.map(({ id, src, caption, nickname, createdAt, ...rest }) =>
     <li key={id} className="communityGrid__element" style={{ background: linearGradient }}>
       <Link to={`/c/${community}/${id}`} className="communityGrid__link">
-        <img src={src ? src : PLACEHOLDER} alt={caption} className="communityGrid__img" />
+        <div className="communityGrid__wrap">
+          <img
+            src={src ? src : PLACEHOLDER}
+            alt={caption}
+            className="communityGrid__img"
+          />
+          {!src &&
+            <div className="communityGrid__spin">
+              <Spinner backgroundColor="#09131D" />
+            </div>
+          }
+        </div>
         <div className="communityGrid__meta">
           <h3 className="communityGrid__caption">{caption}</h3>
           <div className="communityGrid__metadata">
