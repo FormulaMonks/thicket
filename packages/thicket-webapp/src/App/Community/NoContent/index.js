c26ef930824e95d69550c4fe44c9ccbc4b526f16
Gorka <gorka@aquigorka.com>
2018-02-08 15:39:20 +0100

    NoContent: add space between no content text and button

--- packages/thicket-webapp/src/App/Community/NoContent/index.js
+++ packages/thicket-webapp/src/App/Community/NoContent/index.js
@@ -15,7 +15,7 @@ export default ({ onCreate, syncing }) => {
   return <div className="noContent">
     <img src={sadSvg} alt="Sad because there are no GIFs" />
     <h3>This Community doesn’t have content yet!</h3>
-    <div>Create new GIFs to add to the Community or invite new or existing Thicket users to create and contribute GIFs.</div>
+    <div className="noContent__text">Create new GIFs to add to the Community or invite new or existing Thicket users to create and contribute GIFs.</div>
     <Button onClick={onCreate}>Create New GIF</Button>
   </div>
 }
