297f6e62a101ba78a5c180db4195a4c411ac735b
Gorka <gorka@aquigorka.com>
2018-02-08 09:54:00 +0100

    Update default gifs

--- packages/thicket-webapp/src/utils/constants.js
+++ packages/thicket-webapp/src/utils/constants.js
@@ -1,21 +1,21 @@
 export const DEFAULT_PUBLICATIONS = [
   {
-    hash: 'QmcQhBbhFpPdCze5iJ6faws57gYwtRYsuNQLhEdmN6khej',
-    caption: 'Welcome to Thicket',
-    path: '/onboarding-gifs/welcome-to-thicket.gif',
+    hash: 'QmQKooJPWpYY2v7CB4shGKa7UVcbE3Z61sGjibb3aFkXb1',
+    caption: 'Create New GIF',
+    path: '/onboarding-gifs/create-new-gif.gif',
     nickname: 'Thicket',
   },
   {
-    hash: 'QmZmq3cKBZMTrPfH3dgmffsjvgidyC7eVLvZHLNxjX5g9C',
+    hash: 'QmSPJAfNidu4ofwhApeRneYeqpoBoJURWHaopvBnEV6GGp',
     caption: 'Share Your Community',
     path: '/onboarding-gifs/share-your-community.gif',
     nickname: 'Thicket',
   },
   {
-    hash: 'Qmd9YskGYAbo8rnpaDyXdoCVgS1oZFLSERwbT5vdE8rTxp',
-    caption: 'Create New GIF',
-    path: '/onboarding-gifs/create-new-gif.gif',
+    hash: 'QmRTooGFkG19BZMuUXoXYDMKLkeaTXxohPDErcGea9YrD2',
+    caption: 'Welcome to Thicket',
+    path: '/onboarding-gifs/welcome-to-thicket.gif',
     nickname: 'Thicket',
-  }
+  },
 ]
 export const PLACEHOLDER = '/images/placeholder.png'
