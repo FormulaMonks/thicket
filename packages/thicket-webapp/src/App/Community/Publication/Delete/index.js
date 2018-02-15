5acb80314760563b2f57c708f21f5740e57a9c47
Gorka <gorka@aquigorka.com>
2018-02-15 13:37:06 +0100

    Delete: adjust modal styles

--- packages/thicket-webapp/src/App/Community/Publication/Delete/index.js
+++ packages/thicket-webapp/src/App/Community/Publication/Delete/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Button } from 'thicket-elements'
+import { Styles, Button } from 'thicket-elements'
 import './Delete.css'
+
+const { linearGradient } = Styles
 
 export default class Delete extends Component {
 
@@ -10,23 +12,26 @@ export default class Delete extends Component {
 
   render() {
     const { caption, onDelete, onCancel } = this.props
-    return <div className="delete">
-      <h2 className="delete__title">Are you sure?</h2>
-      <div className="delete__msgs">Are you sure you want to delete the GIF titled <span className="delete__caption">{caption}</span>?</div>
-      <div className="delete__msgs">NOTE: this action cannot be undone</div>
-      <div className="delete__btns">
-        <Button
-          className="delete__confirm"
-          onClick={onDelete}
-        >
-          Confirm Delete
-        </Button>
-        <Button
-          secondary
-          onClick={onCancel}
-        >
-          Cancel
-        </Button>
+    return <div className="delete" style={{ background: linearGradient }}>
+      <div className="delete__wrap">
+        <h3>Are you sure?</h3>
+        <div className="delete__msgs">Are you sure you want to delete the GIF titled <span className="delete__caption">"{caption}"</span>?</div>
+        <div className="delete__msgs">
+          <b>NOTE:</b> this action cannot be undone.</div>
+        <div className="delete__btns">
+          <Button
+            className="delete__confirm"
+            onClick={onDelete}
+          >
+            Confirm Delete
+          </Button>
+          <Button
+            secondary
+            onClick={onCancel}
+          >
+            Cancel
+          </Button>
+        </div>
       </div>
     </div>
   }
