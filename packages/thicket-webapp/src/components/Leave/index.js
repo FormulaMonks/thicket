712f98bae50d9af89ad37850f66831083eb5161e
Gorka <gorka@aquigorka.com>
2018-02-15 13:22:56 +0100

    Leave: adjust modal styles

--- packages/thicket-webapp/src/components/Leave/index.js
+++ packages/thicket-webapp/src/components/Leave/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Modal, Button } from 'thicket-elements'
+import { Modal, Button, Styles } from 'thicket-elements'
 import './Leave.css'
 
-export default ({ title, onLeave, onCancel }) => <Modal className="leave" disableBodyScroll>
-  <h3 className="leave__title">Leave Community?</h3>
-  <div className="leave__msgs">Are you sure you want to leave the <span className="leave__community">"{title}"</span> Community?</div>
-  <div className="leave__msgs">Note: You will no longer be able to view or contribute content to this Community. Content will remain in the Community, but you will need to be reinvited to rejoin.</div>
-  <div className="leave__btns">
-    <Button className="leave__btn" onClick={onLeave}>Leave Community</Button>
-    <Button secondary onClick={onCancel}>Cancel</Button>
+const { linearGradient } = Styles
+
+export default ({ title, onLeave, onCancel }) => <Modal
+  className="leave"
+  disableBodyScroll
+  style={{ background: linearGradient }}
+>
+  <div className="leave__wrap">
+    <h3>Leave Community?</h3>
+    <div className="leave__msgs">Are you sure you want to leave the <span className="leave__community">"{title}"</span> Community?</div>
+    <div className="leave__msgs">Note: You will no longer be able to view or contribute content to this Community. Content will remain in the Community, but you will need to be reinvited to rejoin.</div>
+    <div className="leave__btns">
+      <Button className="leave__btn" onClick={onLeave}>Leave Community</Button>
+      <Button secondary onClick={onCancel}>Cancel</Button>
+    </div>
   </div>
 </Modal>
