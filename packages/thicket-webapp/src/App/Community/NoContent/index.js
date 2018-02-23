cf3863304f3b15e44ff2de64c2ba70c4371ee470
Gorka <gorka@aquigorka.com>
2018-02-23 13:45:31 +0100

    Community: apply theme heading rules

--- packages/thicket-webapp/src/App/Community/NoContent/index.js
+++ packages/thicket-webapp/src/App/Community/NoContent/index.js
@@ -7,14 +7,14 @@ export default ({ onCreate, syncing }) => {
   if (syncing) {
     return <div className="communityNoContent">
       <Spinner />
-      <h3>Syncing Community</h3>
+      <h2>Syncing Community</h2>
       <div>Your community is syncing data between the browsers currently online. This may take a moment.</div>
     </div>
   }
 
   return <div className="communityNoContent">
     <img src={sadSvg} alt="Sad because there are no GIFs" />
-    <h3>This Community doesn’t have content yet!</h3>
+    <h2>This Community doesn’t have content yet!</h2>
     <div className="communityNoContent__text">Create new GIFs to add to the Community or invite new or existing Thicket users to create and contribute GIFs.</div>
     <Button onClick={onCreate}>Create GIF</Button>
   </div>
