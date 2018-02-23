3e2eb9ed4bb35e6ebc89d334d9631c8950e9dbd2
Gorka <gorka@aquigorka.com>
2018-02-23 13:47:21 +0100

    Leave: apply theme heading rules

--- packages/thicket-webapp/src/components/Leave/index.js
+++ packages/thicket-webapp/src/components/Leave/index.js
@@ -13,9 +13,9 @@ export default class Leave extends Component {
       style={{ background: linearGradient }}
     >
       <div className="leave__wrap">
-        <h3>Leave Community?</h3>
-        <div className="leave__msgs">Are you sure you want to leave the <span className="leave__community">"{title}"</span> Community?</div>
-        <div className="leave__msgs">Note: You will no longer be able to view or contribute content to this Community. Content will remain in the Community, but you will need to be reinvited to rejoin.</div>
+        <h2>Leave Community?</h2>
+        <h3>Are you sure you want to leave the <span className="leave__community">"{title}"</span> Community?</h3>
+        <div className="leave__msg"><b>Note:</b> You will no longer be able to view or contribute content to this Community. Content will remain in the Community, but you will need to be reinvited to rejoin.</div>
         <div className="leave__btns">
           <Button className="leave__btn" onClick={onLeave}>Leave Community</Button>
           <Button secondary onClick={onCancel}>Cancel</Button>
