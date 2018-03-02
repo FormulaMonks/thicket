4413c38f45482ac5125e29c31b1b93f1fda6afbf
Gorka <gorka@aquigorka.com>
2018-03-02 10:54:17 +0100

    App: use hash router instead of browser router

--- packages/thicket-webapp/src/utils/links.js
+++ packages/thicket-webapp/src/utils/links.js
@@ -1,4 +1,4 @@
-export const getGIFLink = (communityId, gifId) => `${document.location.origin}/g/${communityId}/${gifId}`
+export const getGIFLink = (communityId, gifId) => `${document.location.origin}/#g/${communityId}/${gifId}`
 
 export const getCommunityInviteLink = communityId =>
-  document.location.origin + '/c/' + communityId + '?token=' + btoa(communityId)
+  document.location.origin + '/#c/' + communityId + '?token=' + btoa(communityId)
