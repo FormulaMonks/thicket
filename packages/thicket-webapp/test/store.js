8c5b0972d0362be45e5fa8f2b58cb3145f04af7b
Gorka <gorka@aquigorka.com>
2018-03-27 16:06:07 +0200

    User: add put test

--- packages/thicket-webapp/test/store.js
+++ packages/thicket-webapp/test/store.js
@@ -1,5 +1,9 @@
 import store, { initialState } from '../src/database/store'
 
+beforeEach(() => {
+  localStorage.clear()
+})
+
 describe('Store', () => {
   it('should expose user & communities interface', () => {
     expect(store.hasOwnProperty('user')).toBe(true)
@@ -11,9 +15,16 @@ describe('Store', () => {
       const user = await store.user.get()
       expect(user).toBe(initialState.user)
     })
+    it('should set the user’s nickname to the new value', async () => {
+      const newNickname = 'new nickname'
+      const { user } = store
+      await user.put({ nickname: newNickname })
+      const { nickname } = await user.get()
+      expect(nickname).toBe(newNickname)
+    })
   })
 
-  describe('Comunities', async () => {
+  describe('Communities', async () => {
     it('should return an empty set on the first fetch', async () => {
       const communities = await store.communities.getAll()
       expect(communities.length).toBe(0)
