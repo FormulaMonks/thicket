6e483ec0868757bc15f1877094e3d1e5ace8de20
Gorka <gorka@aquigorka.com>
2018-04-25 09:35:56 +0200

    User: add fix for jest life cycle hook

--- packages/thicket-webapp/src/database/__tests__/user.test.js
+++ packages/thicket-webapp/src/database/__tests__/user.test.js
@@ -5,27 +5,24 @@ const TEST = 'user'
 const mock = options(TEST)
 const { user } = createStore(mock('crud'))
 
-describe('User', () => {
+test('initial state on first fetch', async () => {
+  const data = await user.get()
+  expect(data).toEqual(initialState.user)
+})
 
-  it('should return the initial state on the first fetch', async () => {
-    const data = await user.get()
-    expect(data).toEqual(initialState.user)
-  })
+test('change user’s nickname', async () => {
+  const newNickname = 'new nickname'
+  await user.put({ nickname: newNickname })
+  const { nickname } = await user.get()
+  expect(nickname).toBe(newNickname)
+})
 
-  it('should set the user’s nickname to the new value', async () => {
-    const newNickname = 'new nickname'
-    await user.put({ nickname: newNickname })
+test('receive event after user’s nickname changes', async () => {
+  expect.assertions(1)
+  const newNickname = 'other new nickname'
+  user.once('update', async () => {
     const { nickname } = await user.get()
     expect(nickname).toBe(newNickname)
   })
-
-  it('should emit when user’s data changes', async () => {
-    expect.assertions(1)
-    const newNickname = 'other new nickname'
-    user.once('update', async () => {
-      const { nickname } = await user.get()
-      expect(nickname).toBe(newNickname)
-    })
-    await user.put({ nickname: newNickname })
-  })
+  await user.put({ nickname: newNickname })
 })
