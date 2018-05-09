3d8e14fd653e2c709fcad34acbdf65e163a403b4
Gorka <gorka@aquigorka.com>
2018-05-09 11:59:29 +0200

    Utils: add common flow functions

--- packages/thicket-webapp/src/App/__tests__/e2e/utils.js
+++ packages/thicket-webapp/src/App/__tests__/e2e/utils.js
@@ -75,9 +75,52 @@ export const addPublication = async ({ page, browser, caption, nickname }) => {
   await page.click('[data-test="customize-submit"]')
 }
 
+export const changeProfileNickname = async ({ page, newNickname }) => {
+  await page.click('[data-test="profile-link"]')
+  await page.waitFor('[data-test="profile"]')
+  await page.$eval('[data-test="profile-input"]', input => input.value = '')
+  await page.type('[data-test="profile-input"]', newNickname)
+  await page.click('[data-test="profile-save"]')
+}
+
+export const changePublicationMeta = async ({ page, caption, nickname }) => {
+  await page.waitFor('[data-test="publication-modal"]')
+  await page.waitFor(1000)
+  if (nickname) {
+    await page.click('[data-test="gif-created-by"] [data-test="editable-edit"]')
+    await page.waitFor('[data-test="gif-created-by"] [data-test="editable-input"]')
+    await page.$eval('[data-test="gif-created-by"] [data-test="editable-input"]', input => input.value = '')
+    await page.type('[data-test="gif-created-by"] [data-test="editable-input"]', nickname)
+  }
+  if (caption) {
+    await page.click('[data-test="gif-caption"] [data-test="editable-edit"]')
+    await page.waitFor('[data-test="gif-caption"] [data-test="editable-input"]')
+    await page.$eval('[data-test="gif-caption"] [data-test="editable-input"]', input => input.value = '')
+    await page.type('[data-test="gif-caption"] [data-test="editable-input"]', caption)
+    await page.click('[data-test="publication-save"]')
+  }
+}
+
 export const checkPublicationMeta = async ({ page, index, nickname, caption }) => {
   const nick = await page.$eval(`[data-test="community-grid-meta-${index}"] [data-test="community-grid-nickname"]`, e => e.innerHTML)
   const cap = await page.$eval(`[data-test="community-grid-meta-${index}"] [data-test="community-grid-caption"]`, e => e.innerHTML)
   expect(nick).toBe(nickname)
   expect(cap).toBe(caption)
+}
+
+export const clickOnPublicationByIndex = async ({ page, index }) => {
+  const paused = await page.$$eval(`[data-test="community-grid-wrap-${index}"] [data-test="gif-paused"]`, e => e.length)
+  if (paused) {
+    await page.click(`[data-test="community-grid-wrap-${index}"] .play_button`)
+  }
+  await page.waitFor(500)
+  await page.click(`[data-test="community-grid-wrap-${index}"] [data-test="playable-gif-link"]`)
+}
+
+export const deletePublication = async ({ page, index }) => {
+  await clickOnPublicationByIndex({ page, index })
+  await page.waitFor('[data-test="publication-modal"]')
+  await page.click('[data-test="publication-delete"]')
+  await page.waitFor('[data-test="delete-confirm"]')
+  await page.click('[data-test="delete-confirm"]')
 }
